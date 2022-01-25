17a26187f50c17609a2237819f28c7881f16005d
RamazanFerhatSonmez <rmzns1515@gmail.com>
2022-01-25 21:05:50 +0300

    persons services and interface parts have been created.

---
+++ Persons.Core/IPersonsServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons.Core
+{
+    public interface IPersonsServices
+    {
+        List<Persons> GetPersons();
+    }
+}
---
+++ Persons.Core/Persons.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons.Core
+{
+    public class Persons
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+        public string UUID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+        public string ContactInfoId { get; set; }
+
+    }
+}
