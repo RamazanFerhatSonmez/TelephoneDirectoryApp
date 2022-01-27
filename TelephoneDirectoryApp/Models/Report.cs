1273c8fec4619b679c922f6593a9dd6e1562f9ce
RamazanFerhatSonmez <rmzns1515@gmail.com>
2022-01-27 13:17:30 +0300

    Rapora çıkartma ve raprları listeleme kısımları yazıldı.

---
+++ TelephoneDirectoryApp/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelephoneDirectoryApp.Models
+{
+    public class Report
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UUID { get; set; }
+        public DateTime createdAt { get; set; }
+        public string filePath { get; set; }
+        public string status { get; set; }
+
+    }
+}
